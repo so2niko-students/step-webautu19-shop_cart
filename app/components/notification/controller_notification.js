97edfc2d2b0ad03459c7918ca1c8ac149665d925
Nick Sotula <so2niko@gmail.com>
2020-08-13 19:51:23 +0300

    feat: add infromer : thanks for order

--- app/components/notification/controller_notification.js
+++ app/components/notification/controller_notification.js
@@ -3,10 +3,15 @@ import ViewNotification from "./view_notification.js";
 export default class ControllerNotification{
     constructor({ subscribe, events }){
         subscribe(events.ADD_TO_CART, this.handleAddToCart);
+        subscribe(events.SEND_ORDER, this.handleSendOrder);
         this.view = new ViewNotification();
     }
 
     handleAddToCart = () => {
-        this.view.renderAddToCart();
+        this.view.renderMessage('ADD_TO_CART');
+    }
+
+    handleSendOrder = () => {
+        this.view.renderMessage('THANKS_FOR_ORDER');
     }
 }
